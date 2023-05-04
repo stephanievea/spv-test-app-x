33fd1afa8051fb852a6ab9b9e0eebf5640209e6b
smri-stephanievea <stephanie.p.vea@smretail.com>
2023-05-04 13:34:47 +0800

    cleanup

--- src/pages/characters/index.js
+++ src/pages/characters/index.js
@@ -54,7 +54,7 @@ const Overlay = styled.div`
 const CharacterContainer = styled.div`
   color: #000;
   font-size: 1rem;
-  font-family: monospace;
+  font-family: courier, choplin, 'Andale Mono';
   border-radius: 1rem;
   font-weight: 600;
   display: flex;
@@ -143,8 +143,6 @@ function CharactersPage({characters}) {
 }
 
 // This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
